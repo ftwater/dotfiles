ff7f37eeab5adc8293a46d72a50a932217008fa8
ftwater <zypmain@gmail.com>
2023-09-25 09:22:07 +0800

    add snap-setup.sh

---
+++ snap-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt-get install neovim zoxide ibus-rime librime-data-luna-pinyin -y
+
+
+sudo snap install sublime-text --classic
+
+
+
+sudo snap install intellij-idea-community --classic
+
+
+
+sudo snap install dbeaver-ce
+
+
+
+sudo snap install bitwarden
+
+
+
+sudo snap install code --classic
+
+
+
+sudo snap install obsidian --classic
+
+
+
