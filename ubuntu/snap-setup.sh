eead66d87e68a576eebbdaaf0372e86bf2aa4009
ftwater <zypmain@gmail.com>
2024-01-30 13:42:50 +0800

    update snap-setup

--- ubuntu/snap-setup.sh
+++ ubuntu/snap-setup.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
-sudo apt-get install neovim zoxide ibus-rime librime-data-luna-pinyin -y
-
-
-sudo snap install sublime-text --classic
-
-
-
+# sudo apt-get install neovim zoxide ibus-rime librime-data-luna-pinyin -y
+# sudo snap install sublime-text --classic
 sudo snap install intellij-idea-community --classic
-
-
-
+snap install datagrip --channel=2022.1/stable --classic
+snap install postman --channel=v9/stable
 sudo snap install dbeaver-ce
-
-
-
 sudo snap install bitwarden
-
-
-
 sudo snap install code --classic
-
-
-
 sudo snap install obsidian --classic
-
-
-
