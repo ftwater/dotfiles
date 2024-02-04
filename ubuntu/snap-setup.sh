766734f226f27816e7c1bb5fe8f0332b4299b305
赵延鹏 <zhaoyanpeng@zhaoyanpengdeMacBook-Pro.local>
2024-02-04 14:11:33 +0800

    update ubuntu/snap-setup.sh

--- ubuntu/snap-setup.sh
+++ ubuntu/snap-setup.sh
@@ -4,7 +4,7 @@
 sudo snap install intellij-idea-community --classic
 snap install datagrip --channel=2022.1/stable --classic
 snap install postman --channel=v9/stable
-sudo snap install dbeaver-ce
+# sudo snap install dbeaver-ce
 sudo snap install bitwarden
 sudo snap install code --classic
 sudo snap install obsidian --classic
