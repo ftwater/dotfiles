5ce93c6e1a9e74ead020b5a57474716e5efa44d4
ftwater <zypmain@gmail.com>
2024-02-19 13:00:04 +0800

    add neovim plugin tmp

--- macos/install_neovim.sh
+++ macos/install_neovim.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 brew install git
 brew install neovim
+# 安装包管理器 packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# 安装tmp 用于管理tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
