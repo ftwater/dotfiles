5794110ab2a12aca805a5e6459e4a8d833636a61
ftwater <zypmain@gmail.com>
2024-02-19 14:36:13 +0800

    update windows setup

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,3 +5,17 @@ if(Get-Command scoop -ErrorAction SilentlyContinue){
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 
+scoop bucket add extras
+scoop bucket add nonportable
+scoop bucket add nerd-fonts
+scoop install extras/googlechrome
+scoop install nonportable/virtualbox-np
+scoop install extras/idea
+scoop install extras/vscode
+scoop install nerd-fonts/0xProto-NF
+scoop install nerd-fonts/0xProto-NF-Mono
+scoop install nerd-fonts/0xProto-NF-Propo
+scoop install extras/bitwarden
+scoop install main/git
+scoop install main/oh-my-posh
+scoop install extras/lazy-posh-git
