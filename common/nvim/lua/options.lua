4fb6c0a84f219971b17274f43f30e93da3084cab
ftwater <zypmain@gmail.com>
2024-02-19 09:27:13 +0800

    use tab accept autosuggestion

--- common/nvim/lua/options.lua
+++ common/nvim/lua/options.lua
@@ -13,6 +13,7 @@ vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 vim.opt.number = true -- show absolute number
 vim.opt.relativenumber = true -- add numbers to each line on the left side
 vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
+
 vim.opt.splitbelow = true -- open new vertical split bottom
 vim.opt.splitright = true -- open new horizontal splits right
 -- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
