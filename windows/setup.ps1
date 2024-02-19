82e78659254557388a30000d6a8edc79b6d5655a
ftwater <zypmain@gmail.com>
2024-02-19 14:24:59 +0800

    update windows setup

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,2 +1,7 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+if(Get-Command scoop -ErrorAction SilentlyContinue){
+    Wirte-Host "scoop已安装，跳过"
+}else{
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+
