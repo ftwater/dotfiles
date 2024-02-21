753012dc4394baa4b7f094187d73fb4b688bf7ca
ftwater <zypmain@gmail.com>
2024-02-21 11:09:10 +0800

    add macos java env config

---
+++ macos/install_java_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+brew install wget tar
+if [[ ! -f apache-maven-3.9.6-bin.tar.gz ]];then
+	wget -O apache-maven-3.9.6-bin.tar.gz https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz 
+fi
+if [[ ! -d ~/tools ]];then
+    mkdir -p ~/tools
+fi
+if [[ -f apache-maven-3.9.6-bin.tar.gz ]];then
+	tar -zxvf apache-maven-3.9.6-bin.tar.gz -C ~/tools/ 
+	cp -f ./config/settings.xml ~/tools/apache-maven-3.9.6/conf
+	rm -rf apache-maven-3.9.6-bin.tar.gz
+	echo "maven install success"
+else	
+	echo "maven install faild:apache-maven-3.9.6-bin.tar.gz not exist"
+	return 1
+fi
