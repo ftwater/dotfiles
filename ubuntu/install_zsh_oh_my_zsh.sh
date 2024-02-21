f5d7b9f6e83af7fb26ee1414ead81fee1fcea9af
ftwater <zypmain@gmail.com>
2024-02-21 13:28:23 +0800

    add zsh-autosuggestion

--- ubuntu/install_zsh_oh_my_zsh.sh
+++ ubuntu/install_zsh_oh_my_zsh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo apt-get install curl zsh -y
+# zsh-autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# oh-my-zsh
 echo y |sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
 wait $!
 if [[ $? -ne 0 ]];then
