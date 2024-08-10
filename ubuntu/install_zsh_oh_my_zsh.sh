ef83bdd0bf46b990f7cdd470616920c6573091e1
ftwater <zypmain@gmail.com>
2024-08-10 20:01:07 +0800

    update install_zsh_oh_my_zsh.sh

--- ubuntu/install_zsh_oh_my_zsh.sh
+++ ubuntu/install_zsh_oh_my_zsh.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
+sudo -v
 sudo apt-get install curl zsh -y
+# oh-my-zsh
+echo y |sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+sudo chsh -s $(which zsh) $USER
+echo "oh my zsh install success"
 # zsh-autosuggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# oh-my-zsh
-echo y |sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
-wait $!
-if [[ $? -ne 0 ]];then
-	echo "oh my zsh install faild"
-else
-	sudo chsh -s $(which zsh)
-	echo "oh my zsh install success"
-fi
-
+echo "zsh-autosuggestion install success"
